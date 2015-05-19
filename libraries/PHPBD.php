1cbf93173dd586fbaa31bdbaca888ce09f5add33
irispineda <irispineda85@gmail.com>
2015-05-19 11:04:03 -0600

    metodos para conectarse a la base de datos

---
+++ libraries/PHPBD.php
@@ -0,0 +1,28 @@
+<?php
+	class PHPBD{
+		//variable para el enlace a la bd
+		private $link;
+		
+		// Conectando, seleccionando la base de datos
+		public function conectar(){
+			$this->link = mysqli_connect('localhost', 'root', '', 'sig') 
+			or die('No se pudo conectar: ' . mysql_error());
+		}
+		
+		// Cerrar la conexión
+		public function cerrar(){
+			mysqli_close($this->link);
+		}
+		
+		public function consultar($query){
+			$result = mysqli_query($this->link,$query) or die('Consulta fallida: ' . mysql_error());
+			
+			return $result;
+		}
+		
+		// Liberar resultados
+		public function liberar($result){
+			mysqli_free_result($result);
+		}
+	}
+?>
